9e2d5f147b8fffd7a8145cd67add46f436ffe5d0
IsaiahSama <zeldaplayergl@gmail.com>
2024-03-28 16:39:53 -0400

    Added routing for application

--- VN2Text/app/index.js
+++ VN2Text/app/index.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const listItems = [
   {
