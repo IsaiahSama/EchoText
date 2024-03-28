4a3430d49c4f7abf5f53f9682a1b9b1918f304ad
IsaiahSama <zeldaplayergl@gmail.com>
2024-03-28 16:52:47 -0400

    Added global stylesheet

---
+++ VN2Text/app/appstyle.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const appStyle = StyleSheet.create({
+    container: {
+        margin: 5,
+        marginTop: 40,
+        backgroundColor: '#fff',
+        flex: 1,
+        justifyContent: 'space-between'
+    },
+    heading : {
+        fontSize: 30,
+        textAlign: 'center',
+    },
+    content: {
+        fontSize: 20,
+    },
+})
+
+export default appStyle;
